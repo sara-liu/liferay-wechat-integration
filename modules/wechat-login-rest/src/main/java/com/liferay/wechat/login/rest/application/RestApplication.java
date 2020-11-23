dbc28cbe656ebbf699eea5b304d2c74506e5c3a1
Sara Liu <sara.liu@liferay.com>
2020-11-23 22:06:13 +0800

    continue

--- modules/wechat-login-rest/src/main/java/com/liferay/wechat/login/rest/application/RestApplication.java
+++ modules/wechat-login-rest/src/main/java/com/liferay/wechat/login/rest/application/RestApplication.java
@@ -1,7 +1,6 @@
 package com.liferay.wechat.login.rest.application;
 
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -36,10 +35,17 @@ import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
  * @author SaraLiu
  * 
  */
-@Component(property = { JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/wechat",
-		JaxrsWhiteboardConstants.JAX_RS_NAME + "=WechatLogin" }, service = Application.class)
+
+@Component(
+	property = { 
+			JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/wechat",
+			JaxrsWhiteboardConstants.JAX_RS_NAME + "=WechatLogin" 
+	},
+	service = Application.class
+)
 
 public class RestApplication extends Application {
+
 	Log logger = LogFactoryUtil.getLog(RestApplication.class);
 
 	private static final String SECRET_PROP_FILE_NAME = "secret.properties";
@@ -68,15 +74,10 @@ public class RestApplication extends Application {
 //	}
 
 	// NOTE: This method is necessary for Liferay Protal, don't delete it.
-	public Set<Object> getSingletons() {
-		return Collections.<Object>singleton(this);
-	}
 
-	@GET
-	@Path("/hello")
-	@Produces("text/plain")
-	public String hello() {
-		return "hello, this works!";
+	public Set<Object> getSingletons() {
+
+		return Collections.<Object>singleton(this);
 	}
 
 	@GET
@@ -84,14 +85,15 @@ public class RestApplication extends Application {
 	@Produces("text/plain")
 	public String wechatLogin(@Context HttpServletRequest request, @Context HttpServletResponse response)
 			throws Exception {
+
 		response.setCharacterEncoding("utf-8");
 		String code = request.getParameter("code");
 		AccessTokenInfo accessToken = getAccessToken(code);
 		UserInfo userInfo = getUserInfo(accessToken);
 
-		// NOTES: the nickname cannot be Chinese (DefaultScreenNameValidator will throw an exception)
 		User user = null;
 		boolean newAdded = false;
+
 		try {
 			// Wechat opendId is used as the screen name
 			user = UserLocalServiceUtil.getUserByScreenName(companyId, accessToken.openId);
@@ -130,24 +132,23 @@ public class RestApplication extends Application {
 
 	private class UserInfo {
 		String nickName;
-		String sex;
-		String province;
-		JSONArray privilege;
 	}
 
 	private User addUser(String openId, String nickName) throws PortalException {
+
 		long[] empty_long_list = {};
 
 		// Use wechat opendId as the screen name
 		User user = UserLocalServiceUtil.addUser(20130, companyId, false, defaultPassword, defaultPassword, false,
-				openId, openId + "@liferay.com", 0, "", java.util.Locale.CHINA, nickName, "", lastName, 0, 0, true, 1,
-				1, 1970, "", empty_long_list, empty_long_list, empty_long_list, empty_long_list, false,
-				new ServiceContext());
+			openId, openId + "@liferay.com", 0, "", java.util.Locale.CHINA, nickName, "", lastName, 0, 0, true, 1,
+			1, 1970, "", empty_long_list, empty_long_list, empty_long_list, empty_long_list, false,
+			new ServiceContext());
 
 		return user;
 	}
 
 	private AccessTokenInfo getAccessToken(String code) throws JSONException, IOException {
+
 		String url = "https://api.weixin.qq.com/sns/oauth2/access_token";
 		StringBuilder sb = new StringBuilder();
 		sb.append(url).append("?").append("appid=").append(APP_ID).append("&secret=").append(SECRET).append("&code=")
@@ -162,10 +163,12 @@ public class RestApplication extends Application {
 		AccessTokenInfo info = new AccessTokenInfo();
 		info.accessToken = accessToken;
 		info.openId = openId;
+
 		return info;
 	}
 
 	private UserInfo getUserInfo(AccessTokenInfo info) throws JSONException, IOException {
+
 		String url = "https://api.weixin.qq.com/sns/userinfo";
 		String accessToken = info.accessToken;
 		String openId = info.openId;
@@ -182,13 +185,16 @@ public class RestApplication extends Application {
 
 		UserInfo userInfo = new UserInfo();
 		userInfo.nickName = jsonObject.getString("nickname");
-		userInfo.sex = jsonObject.getString("sex");
-		userInfo.province = jsonObject.getString("province");
+		/*
+		 * userInfo.sex = jsonObject.getString("sex"); userInfo.province =
+		 * jsonObject.getString("province");
+		 */
 
 		return userInfo;
 	}
 
 	public String sendHttpGet(String url) throws JSONException, IOException {
+
 		HttpClient client = new HttpClient();
 		GetMethod method = new GetMethod(url);
 
@@ -205,6 +211,7 @@ public class RestApplication extends Application {
 
 			// Use caution: ensure correct character encoding and is not binary data
 			String result = new String(responseBody, "utf-8");
+
 			return result;
 		} catch (HttpException e) {
 			logger.error("Fatal protocol violation: " + e.getMessage());
--- modules/wechat-login-web/src/main/resources/META-INF/resources/view.jsp
+++ modules/wechat-login-web/src/main/resources/META-INF/resources/view.jsp
@@ -1,25 +1,15 @@
 <%@ include file="/init.jsp"%>
 
 <p>
-  <b><liferay-ui:message key="wechatlogin.caption" /></b>
+<% 
+if (themeDisplay.isSignedIn()) {
+%>
+  <liferay-ui:message key="Welcome"/> <%= themeDisplay.getRealUser().getFirstName() %> 
 
-  <%-- 	
-	<portlet:actionURL name="/login/view" var="UserAuth" />
-	<aui:a href="<%= UserAuth.toString() %>" label="login-by-wechat" />
-	 --%>
-
-<a
-    href="https://open.weixin.qq.com/connect/qrconnect?appid=wx2d653b1ce6779c0e&redirect_uri=http%3A%2F%2Fwww.liferaydemo.cn/o/wechat/wechat_login&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect">
-    Login by Wechat </a>   
-	 
-<%-- <portlet:renderURL var="loginByWechat">
-		<portlet:param name="mvcRenderCommandName" value="https://open.weixin.qq.com/connect/qrconnect?appid=wx2d653b1ce6779c0e&redirect_uri=http%3A%2F%2Fwww.liferaydemo.cn/o/wechat/sendCode&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect" />
-		<!-- <portlet:param name="mvcPath" value="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx74ac7a84f8421a27&redirect_uri=http%3A%2F%2Fsaraliferaydemo.nat300.top/o/wx/sendCode&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect" /> -->
-		<portlet:param name="redirect" value="<%= currentURL %>" />
-	</portlet:renderURL>
-
-	<aui:button-row>
-		<aui:button href="<%= loginByWechat %>" value="login-by-wechat" />
-	</aui:button-row> --%>
+<% } else { %>
+	<a
+	href="https://open.weixin.qq.com/connect/qrconnect?appid=wx2d653b1ce6779c0e&redirect_uri=http%3A%2F%2Fwww.liferaydemo.cn/o/wechat/wechat_login&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect">
+	Login by Wechat </a>
 	
+<% } %>
 </p>
